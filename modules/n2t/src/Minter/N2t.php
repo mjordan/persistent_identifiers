a2f79f8c90efaebb062d365dfb985a9a4fd03f17
Mark Jordan <mjordan@sfu.ca>
2021-07-06 05:44:25 +0000

    Initial commit for adding n2t ARK support.

---
+++ modules/n2t/src/Minter/N2t.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\n2t\Minter;
+
+use Drupal\persistent_identifiers\MinterInterface;
+use GuzzleHttp\Exception\RequestException;
+
+/**
+ * A Handle class.
+ *
+ * Mints a persistent identifier using a configurable
+ * namespace and a random string.
+ */
+class N2t implements MinterInterface {
+
+  /**
+   * Returns the minter's name.
+   *
+   * @return string
+   *   Appears in the Persistent Identifiers config form.
+   */
+  public function getName() {
+    return t('N2T ARK Minter');
+  }
+
+  /**
+   * Returns the minter's type.
+   *
+   * @return string
+   *   Appears in the entity edit form next to the checkbox.
+   */
+  public function getPidType() {
+    return t('ARK');
+  }
+
+  /**
+   * Mints the identifier.
+   *
+   * Issues a mint request to the EZID service for an ARK.
+   *
+   * @param object $entity
+   *   The entity.
+   * @param mixed $extra
+   *   Extra data the minter needs, for example from the node edit form.
+   *
+   * @return string
+   *   The identifier.
+   */
+  public function mint($entity, $extra = NULL) {
+    $config = \Drupal::config('n2t.settings');
+    $n2t_user = $config->get('n2t_user');
+    $n2t_password = $config->get('n2t_password');
+    $n2t_api_endpoint = $config->get('n2t_api_endpoint');
+    $n2t_shoulder = $config->get('n2t_shoulder');
+
+    // First we mint the ARK.
+    $minting_url = rtrim($n2t_api_endpoint, '/') . '/a/' . $n2t_user . '/m/ark/' . $n2t_shoulder . '?mint%201';
+    $client = \Drupal::httpClient();
+    try {
+      $request = $client->request(
+        'GET',
+        $minting_url,
+	['auth' => [$n2t_user, $n2t_password]]
+      );
+      $response_message = (string) $request->getBody();
+      $parts = preg_split("/\\r\\n|\\r|\\n/", $response_message);
+      if (strpos($parts[0], "s: ") === 0) {
+        $ark = substr($parts[0], 3);
+      }
+    }
+    catch (RequestException $e) {
+      $message = "Minting response: " . (string) $request->getBody() . " Exception message: " . $e->getMessage();
+      \Drupal::logger('persistent identifiers')->error(preg_replace('/Authorization: Basic \w+/', 'Authentication Redacted', $message));
+      return FALSE;
+    }
+
+    // Then we bind it to the current node.
+    $node_host = \Drupal::request()->getSchemeAndHttpHost();
+    // $node_url = $node_host . $entity->toUrl()->toString();
+    $node_url = 'https://www.lib.sfu.ca/facilities/rooms-spaces/library-spaces';
+    $binding_url = rtrim($n2t_api_endpoint, '/') . '/a/' . $n2t_user . '/b?ark:/' . $ark . '.set%20_t%20' . urlencode($node_url);
+    $client = \Drupal::httpClient();
+    try {
+      $request = $client->request(
+        'GET',
+        $binding_url,
+	['auth' => [$n2t_user, $n2t_password]]
+      );
+      $response_message = (string) $request->getBody();
+      if ($response_message == trim('egg-status: 0')) {
+        return rtrim($n2t_api_endpoint, '/') . '/ark:/' . $ark;
+      }
+      else {
+        \Drupal::logger('persistent identifiers')->error('Could not bind ARK; ARK resolver response: ' . $response_message);
+        return FALSE;
+      }
+    }
+    catch (RequestException $e) {
+      $message = "Binding response: " . (string) $request->getBody() . " Exception message: " . $e->getMessage();
+      \Drupal::logger('persistent identifiers')->error(preg_replace('/Authorization: Basic \w+/', 'Authentication Redacted', $message));
+      return FALSE;
+    }
+  }
+
+}
---
+++ modules/n2t/src/Plugin/Action/MintN2tAction.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\n2t\Plugin\Action;
+
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Mints ARKs via N2T.
+ *
+ * @Action(
+ *   id = "n2t_mint_ark",
+ *   label = @Translation("Mint N2T Ark"),
+ *   type = "",
+ *   confirm = TRUE,
+ * )
+ */
+class MintN2tAction extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    $minter = \Drupal::service('n2t.minter.n2t');
+    $persister_id = \Drupal::config('persistent_identifiers.settings')->get('persistent_identifiers_persister');
+    $persister = \Drupal::service($persister_id);
+    // The values saved in this action's configuration form are in $this->configuration.
+    $ark = $minter->mint($entity, $this->configuration);
+
+    if (strlen($ark)) {
+      $persister->persist($entity, $ark);
+      $this->messenger()->addMessage('"' . $entity->label() . '" assigned ' . $ark);
+    }
+    else {
+      $this->messenger()->addMessage('"' . $entity->label() . '" (node ' . $entity->id() . ') not assigned an ARK. See system log for details.');
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    /** @var \Drupal\node\NodeInterface $object */
+    $result = $object->access('edit', $account, TRUE)
+      ->andIf($object->access('update', $account, TRUE));
+
+    return $return_as_object ? $result : $result->isAllowed();
+  }
+
+}
